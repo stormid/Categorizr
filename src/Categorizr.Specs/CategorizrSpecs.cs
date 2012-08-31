0d0e241a184d3f6b4716227494908ccef2fe7c5c
Rory Fitzpatrick <rory@stormid.com>
2012-08-31 14:17:35 +0100

    Initial code port

---
+++ src/Categorizr.Specs/CategorizrSpecs.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Machine.Specifications;
+
+namespace Categorizr.Specs
+{
+    public class CategorizrSpecs
+    {
+        [Subject(typeof(Categorizr))]
+        public class when_testing_user_agents
+        {
+            private static Categorizr subject;
+            private static IEnumerable<CategorizrTest> tests;
+
+            Establish context = () =>
+                {
+                    subject = new Categorizr();
+                    var useragents = File.ReadAllLines("useragents.csv");
+                    var tests = new List<CategorizrTest>();
+                    foreach (var useragent in useragents)
+                    {
+                        var match = Regex.Match(useragent, "^\"(?<ua>[^\"]+)\",(?<expected>[\\w]+)$");
+                        tests.Add(new CategorizrTest
+                        {
+                            Expected = (DeviceCategory)Enum.Parse(typeof(DeviceCategory), match.Groups["expected"].Value),
+                            UserAgent = match.Groups["ua"].Value
+                        });
+                    }
+                    when_testing_user_agents.tests = tests;
+                };
+
+            Because of = () =>
+                {
+                    foreach (var test in tests)
+                    {
+                        subject.UserAgent = test.UserAgent;
+                        subject.Detect();
+                        test.Actual = subject.DetectedCategory;
+                    }
+                };
+
+            It should_detect_correctly = () =>
+                {
+                    foreach (var test in tests)
+                    {
+                        test.Verify();
+                    }
+                };
+        }
+
+        [Subject(typeof(Categorizr))]
+        public class when_categorizing_tablets_as_desktop
+        {
+            private static Categorizr subject;
+
+            Establish context = () =>
+                {
+                    subject = new Categorizr("Mozilla/4.0 (compatible; Linux 2.6.10) NetFront/3.3 Kindle/1.0 (screen 600x800)");
+                    Categorizr.CategorizeTabletsAsDesktops = true;
+                };
+
+            Because of = () => subject.Detect();
+
+            It should_be_desktop = () => subject.DetectedCategory.ShouldEqual(DeviceCategory.Desktop);
+        }
+
+        [Subject(typeof(Categorizr))]
+        public class when_categorizing_tvs_as_desktop
+        {
+            private static Categorizr subject;
+
+            Establish context = () =>
+                {
+                    subject = new Categorizr("Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.127 Large Screen Safari/533.4 GoogleTV/b54202");
+                    Categorizr.CategorizeTvsAsDesktops = true;
+                };
+
+            Because of = () => subject.Detect();
+
+            It should_be_desktop = () => subject.DetectedCategory.ShouldEqual(DeviceCategory.Desktop);
+        }
+    }
+
+    public class CategorizrTest
+    {
+        public string UserAgent { get; set; }
+        public DeviceCategory Expected { get; set; }
+        public DeviceCategory Actual { get; set; }
+        public void Verify()
+        {
+            if (Actual != Expected)
+            {
+                throw new SpecificationException(string.Format(@"User-Agent: {0}
+Expected: [{1}]
+But was:  [{2}]", UserAgent, Expected, Actual));
+            }
+        }
+    }
+}
---
+++ src/Categorizr/Categorizr.cs
@@ -0,0 +1,174 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Categorizr
+{
+    public class Categorizr
+    {
+        private string useragent;
+        private bool hasDetected;
+        private DeviceCategory detected;
+
+        public static bool CategorizeTabletsAsDesktops { get; set; }
+        public static bool CategorizeTvsAsDesktops { get; set; }
+
+        static Categorizr()
+        {
+            CategorizeTabletsAsDesktops = false;
+            CategorizeTvsAsDesktops = false;
+        }
+
+        public Categorizr() { }
+
+        public Categorizr(string useragent)
+        {
+            UserAgent = useragent;
+        }
+
+        public string UserAgent
+        {
+            get { return useragent; }
+            set
+            {
+                useragent = value;
+                hasDetected = false;
+            }
+        }
+
+        public DeviceCategory DetectedCategory 
+        {
+            get
+            {
+                if (!hasDetected)
+                {
+                    Detect();
+                }
+                return detected; 
+            }
+            private set { detected = value; }
+        }
+
+        public bool IsMobile { get { return DetectedCategory == DeviceCategory.Mobile; } }
+
+        public bool IsTablet { get { return DetectedCategory == DeviceCategory.Tablet; } }
+
+        public bool IsDesktop { get { return DetectedCategory == DeviceCategory.Desktop; } }
+
+        public bool IsTv { get { return DetectedCategory == DeviceCategory.Tv; } }
+
+        public void Detect()
+        {
+            if (hasDetected)
+            {
+                return;
+            }
+
+            // Check if user agent is a smart TV - http://goo.gl/FocDk
+            if (Regex.IsMatch(useragent, "GoogleTV|SmartTV|Internet.TV|NetCast|NETTV|AppleTV|boxee|Kylo|Roku|DLNADOC|CE\\-HTML", RegexOptions.IgnoreCase))
+            {
+                DetectedCategory = DeviceCategory.Tv;
+            }
+
+            // Check if user agent is a TV Based Gaming Console
+            else if (Regex.IsMatch(useragent, "Xbox|PLAYSTATION.3|Wii", RegexOptions.IgnoreCase))
+            {
+                DetectedCategory = DeviceCategory.Tv;
+            }
+
+            // Check if user agent is a Tablet
+            else if (Regex.IsMatch(useragent, "iP(a|ro)d", RegexOptions.IgnoreCase) || Regex.IsMatch(useragent, "tablet", RegexOptions.IgnoreCase) && !Regex.IsMatch(useragent, "RX-34", RegexOptions.IgnoreCase) || Regex.IsMatch(useragent, "FOLIO", RegexOptions.IgnoreCase))
+            {
+                DetectedCategory = DeviceCategory.Tablet;
+            }
+
+            // Check if user agent is an Android Tablet
+            else if (Regex.IsMatch(useragent, "Linux", RegexOptions.IgnoreCase) && Regex.IsMatch(useragent, "Android", RegexOptions.IgnoreCase) && !Regex.IsMatch(useragent, "Fennec|mobi|HTC.Magic|HTCX06HT|Nexus.One|SC-02B|fone.945", RegexOptions.IgnoreCase))
+            {
+                DetectedCategory = DeviceCategory.Tablet;
+            }
+    
+            // Check if user agent is a Kindle or Kindle Fire
+            else if (Regex.IsMatch(useragent, "Kindle", RegexOptions.IgnoreCase) || Regex.IsMatch(useragent, "Mac.OS", RegexOptions.IgnoreCase) && Regex.IsMatch(useragent, "Silk", RegexOptions.IgnoreCase))
+            {
+                DetectedCategory = DeviceCategory.Tablet;
+            }
+    
+            // Check if user agent is a pre Android 3.0 Tablet
+            // TODO this doesn't make sense, either of the first two AND the last, or the first one OR both of the last two?
+            else if (Regex.IsMatch(useragent, "GT-P10|SC-01C|SHW-M180S|SGH-T849|SCH-I800|SHW-M180L|SPH-P100|SGH-I987|zt180|HTC(.Flyer|_Flyer)|Sprint.ATP51|ViewPad7|pandigital(sprnova|nova)|Ideos.S7|Dell.Streak.7|Advent.Vega|A101IT|A70BHT|MID7015|Next2|nook", RegexOptions.IgnoreCase) || Regex.IsMatch(useragent, "MB511", RegexOptions.IgnoreCase) && Regex.IsMatch(useragent, "RUTEM", RegexOptions.IgnoreCase))
+            {
+                DetectedCategory = DeviceCategory.Tablet;
+            }
+    
+            // Check if user agent is unique Mobile User Agent
+            else if (Regex.IsMatch(useragent, "BOLT|Fennec|Iris|Maemo|Minimo|Mobi|mowser|NetFront|Novarra|Prism|RX-34|Skyfire|Tear|XV6875|XV6975|Google.Wireless.Transcoder", RegexOptions.IgnoreCase))
+            {
+                DetectedCategory = DeviceCategory.Mobile;
+            }
+    
+            // Check if user agent is an odd Opera User Agent - http://goo.gl/nK90K
+            else if ((Regex.IsMatch(useragent, "Opera", RegexOptions.IgnoreCase)) && (Regex.IsMatch(useragent, "Windows.NT.5", RegexOptions.IgnoreCase)) && (Regex.IsMatch(useragent, "HTC|Xda|Mini|Vario|SAMSUNG\\-GT\\-i8000|SAMSUNG\\-SGH\\-i9", RegexOptions.IgnoreCase)))
+            {
+                DetectedCategory = DeviceCategory.Mobile;
+            }
+    
+            // Check if user agent is Windows Desktop
+            else if ((Regex.IsMatch(useragent, "Windows.(NT|XP|ME|9)", RegexOptions.IgnoreCase)) && (!Regex.IsMatch(useragent, "Phone", RegexOptions.IgnoreCase)) || (Regex.IsMatch(useragent, "Win(9|.9|NT)", RegexOptions.IgnoreCase)))
+            {
+                DetectedCategory = DeviceCategory.Desktop;
+            }
+    
+            // Check if agent is Mac Desktop
+            else if ((Regex.IsMatch(useragent, "Macintosh|PowerPC", RegexOptions.IgnoreCase)) && (!Regex.IsMatch(useragent, "Silk", RegexOptions.IgnoreCase)))
+            {
+                DetectedCategory = DeviceCategory.Desktop;
+            }
+    
+            // Check if user agent is a Linux Desktop
+            else if ((Regex.IsMatch(useragent, "Linux", RegexOptions.IgnoreCase)) && (Regex.IsMatch(useragent, "X11", RegexOptions.IgnoreCase)))
+            {
+                DetectedCategory = DeviceCategory.Desktop;
+            }
+    
+            // Check if user agent is a Solaris, SunOS, BSD Desktop
+            else if ((Regex.IsMatch(useragent, "Solaris|SunOS|BSD", RegexOptions.IgnoreCase)))
+            {
+                DetectedCategory = DeviceCategory.Desktop;
+            }
+    
+            // Check if user agent is a Desktop BOT/Crawler/Spider
+            else if ((Regex.IsMatch(useragent, "Bot|Crawler|Spider|Yahoo|ia_archiver|Covario-IDS|findlinks|DataparkSearch|larbin|Mediapartners-Google|NG-Search|Snappy|Teoma|Jeeves|TinEye", RegexOptions.IgnoreCase)) && (!Regex.IsMatch(useragent, "Mobile", RegexOptions.IgnoreCase)))
+            {
+                DetectedCategory = DeviceCategory.Desktop;
+            }
+    
+            // Otherwise assume it is a Mobile Device
+            else
+            
+            {
+                DetectedCategory = DeviceCategory.Mobile;
+            }
+
+            hasDetected = true;
+
+            // If we categorize tablets as desktops
+            if (CategorizeTabletsAsDesktops && DetectedCategory == DeviceCategory.Tablet)
+            {
+                DetectedCategory = DeviceCategory.Desktop;
+            }
+    
+            // If we categorize tvs as desktops
+            if (CategorizeTvsAsDesktops && DetectedCategory == DeviceCategory.Tv)
+            {
+                DetectedCategory = DeviceCategory.Desktop;
+            }
+        }
+    }
+
+    public enum DeviceCategory
+    {
+        Desktop,
+        Tablet,
+        Mobile,
+        Tv
+    }
+}
