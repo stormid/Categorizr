b1d159955090651fcd6c443d40547a5e845af2af
Rory Fitzpatrick <rory@stormid.com>
2012-09-18 17:27:42 +0100

    Updates build script for nuget publishing (fixes #1)

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,30 +1,70 @@
 require 'albacore'
 require 'fileutils'
 
+task :default => :build
+
 task :build => ['build:specs']
 
 namespace :build do
-	msbuild :msbuild, [:target, :options] => :clean do |msb, args|
-		msbuild_options = args[:options] || {}
+	msbuild :compile, [:target] => ['clean:build'] do |msb, args|
 		msb.targets [:clean, :rebuild]
-		msb.properties = msbuild_options.merge({
+		msb.properties = {
 			:configuration => args[:target] || :Debug,
 			:outdir => "#{Dir.pwd}/build/"
-		})
+		}
 		msb.verbosity = "minimal"
 		msb.solution = "Categorizr.sln"
 	end
 	
-	mspec :specs => :msbuild do |mspec|
+	mspec :specs => :compile do |mspec|
 		mspec.command = "packages/Machine.Specifications.0.5.8/tools/mspec-clr4.exe"
 		mspec.assemblies "build/Categorizr.Specs.dll"
 	end
 end
 
-task :clean do
-	FileUtils.rm_rf("build") if File.directory? "build"
+namespace :clean do
+	task :build do
+		rm_rf("build") if File.directory? "build"
+	end
+	
+	task :release do
+		rm_rf("release") if File.directory? "release"
+	end
 end
 
-task :publish do
+task :publish, [:apikey] => ['clean:release'] do |task, args|
+	Rake::Task['build:compile'].execute({:target => :Release})
+	mkdir_p("release/lib/net20")
+	cp "build/Categorizr.dll", "release/lib/net20"
+	Rake::Task['nuget:create'].invoke
+	Rake::Task['nuget:pack'].invoke
+	Rake::Task['nuget:push'].invoke(args.apikey)
+end
 
+namespace :nuget do
+	nuspec :create do |nuspec|
+		nuspec.id = "Categorizr"
+		nuspec.version = "0.0.1"
+		nuspec.authors = "Rory Fitzpatrick"
+		nuspec.description = "A modern device detection script"
+		nuspec.title = "Categorizr"
+		nuspec.projectUrl = "https://github.com/roryf/Categorizr"
+		nuspec.tags = "mobile device-detection"
+		nuspec.working_directory = "release"
+		nuspec.output_file = "Categorizr.nuspec"
+		nuspec.copyright = "Rory Fitzpatrick 2012, Brett Jankford 2011"
+	end
+	
+	nugetpack :pack do |nuget|
+		nuget.command = ".nuget/Nuget.exe"
+		nuget.nuspec = "release/Categorizr.nuspec"
+		nuget.base_folder = "release"
+		nuget.output = "release"
+	end
+	
+	nugetpush :push, [:apikey] do |nuget, args|
+		nuget.command = ".nuget/Nuget.exe"
+		nuget.package = "release\\Categorizr.0.0.1.nupkg"
+		nuget.apikey = args.apikey
+	end
 end
