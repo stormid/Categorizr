f89086340d2f9324b56a6099a6a66c972debc5fe
Chris Canal <chris.canal@stormid.com>
2013-01-09 16:13:33 +0000

    Adds an Mvc usage example

---
+++ Mvc/App_Start/CategorizrConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.WebPages;
+
+namespace Mvc
+{
+    public class CategorizrConfig
+    {
+        public static void Register(DisplayModeProvider instance)
+        {
+            var categorizr = new Categorizr.Impl.Categorizr();
+
+            instance.Modes.Insert(0, new DefaultDisplayMode("Mobile")
+                {
+                    ContextCondition = c => categorizr.Detect(c.GetOverriddenUserAgent()).IsMobile
+                });
+
+            instance.Modes.Insert(1, new DefaultDisplayMode("Tablet")
+                {
+                    ContextCondition = c => categorizr.Detect(c.GetOverriddenUserAgent()).IsTablet
+                });
+            
+            instance.Modes.Insert(2, new DefaultDisplayMode("TV")
+                {
+                    ContextCondition = c => categorizr.Detect(c.GetOverriddenUserAgent()).IsTv
+                });
+        }
+    }
+}
